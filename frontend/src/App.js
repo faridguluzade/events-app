6307144bb5c6be9771c736d8e2fe25cd6281259e
Farid <faridguluzade26@gmail.com>
2023-04-05 22:47:37 -0800

    using react router and their loader and action future to interact with backend

---
+++ frontend/src/App.js
@@ -0,0 +1,69 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+import EventsPage, { eventsLoader } from "./pages/Events";
+import EventDetailPage, {
+  eventDetailLoader,
+  deleteEventAction,
+} from "./pages/EventDetail";
+import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
+import RootLayout from "./pages/Root";
+import EventsRootLayout from "./pages/EventsRoot";
+import ErrorPage from "./pages/Error";
+import { manipulateEventAction } from "./components/EventForm";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: "/events",
+        element: <EventsRootLayout />,
+        children: [
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
+
+          {
+            path: ":eventId",
+            id: "event-detail",
+            loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+                action: deleteEventAction,
+              },
+              {
+                path: "edit",
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
+            ],
+          },
+
+          {
+            path: "new",
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/EventDetail.jsx
@@ -0,0 +1,44 @@
+import { json, redirect, useRouteLoaderData } from "react-router-dom";
+
+import EventItem from "../components/EventItem";
+
+const EventDetailPage = () => {
+  const data = useRouteLoaderData("event-detail");
+
+  return <EventItem event={data.event} />;
+};
+
+export default EventDetailPage;
+
+////////////////////////
+
+// loader function
+export const eventDetailLoader = async ({ request, params }) => {
+  const id = params.eventId;
+
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch details for selected event." },
+      { status: 500 }
+    );
+  } else {
+    return response;
+  }
+};
+
+// Action function
+export const deleteEventAction = async ({ params, request }) => {
+  const eventId = params.eventId;
+
+  const response = await fetch("http://localhost:8080/events/" + eventId, {
+    method: request.method,
+  });
+
+  if (!response.ok) {
+    throw json({ message: "Could not delete event." }, { status: 500 });
+  }
+
+  return redirect("/events");
+};
